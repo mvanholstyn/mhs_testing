553a88e9a8fd84ae9eab39b69b5788fd8334d679
Mark Van Holstyn <mvanholstyn@gmail.com>
2008-01-30 22:43:24 +0000

    Dont try to configure rspec yet

--- lib/lwt_testing.rb
+++ lib/lwt_testing.rb
@@ -1,5 +1,5 @@
 require File.join(File.dirname(__FILE__), 'matchers')
 
-Spec::Runner.configure do |config|
-  config.include ValidationMatchers, AssociationMatchers
-end
+# Spec::Runner.configure do |config|
+#   config.include ValidationMatchers, AssociationMatchers
+# end
